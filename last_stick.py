789e1c86879ca6233a980d9c147f34c3c2d8f1ba
gustavozf <gzf1996@gmail.com>
2019-06-26 03:30:24 +0000

    updates

--- last_stick.py
+++ last_stick.py
@@ -10,11 +10,14 @@ def init_pyramid(n=4):
 
 def get_total(pyramid):
     return pyramid.sum()
+
+def get_valid_lines(pyramid):
+    return np.argwhere(pyramid > 0)[:,0]
 # --------------------------------------------------- HEURISTICAS
 def h_easy(pyramid):
     
     # pega todos os argumentos nao nulos
-    valid_lines = np.argwhere(pyramid > 0)[:,0]
+    valid_lines = get_valid_lines(pyramid)
     # escolhe um aleatorio
     l = random.choice(valid_lines)
     # remove um valor entre 1 e TOTAL
@@ -27,8 +30,8 @@ def h_med(pyramid):
     valid_lines = get_valid_lines(pyramid)
     l = np.argmax(pyramid)
 
-    # se for par
-    if len(valid_lines) & 1:
+    # se for impar e se o num de palitos for maior que um
+    if (len(valid_lines) & 1) and (pyramid[l] > 1):
         # pega o maior e tira o total -1
         pyramid[l] = 1
     else:
